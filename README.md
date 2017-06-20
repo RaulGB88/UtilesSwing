9297dafbab5318ca521d75c1e9781c807b6297ac
RaulGB8 <raul.granel@gmail.com>
2017-06-20 09:42:08 +0200

    Version 0.1.0
    Swing Tools

---
+++ README.md
@@ -0,0 +1,11 @@
+<pre>
+Project .....: UtilesSwing
+Summary .....: Swing Tools
+Creation ....: 12/05/2017
+Modification : 
+Version .....: 0.1.0
+Author ......: Raúl Granel Blasco - raul.granel@gmail.com
+Platform ....: Java 8
+IDE .........: NetBeans 8.2
+Remote ......: https://github.com/RaulGB88/UtilesSwing.git
+</pre>
---
+++ src/library/UtilesSwing.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package library;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author (c) Raúl Granel Blasco - raul.granel@gmail.com
+ */
+public class UtilesSwing {
+
+    // Cerrar programa
+    public static void terminarPrograma(JFrame f) {
+        // Oculta la ventana
+        f.setVisible(false);
+
+        // Devuelve los recursos
+        f.dispose();
+
+        // Cierra el programa
+        System.exit(0);
+    }
+
+}
